623cfa732b22f6c389034e72aa3a115cb43c7be7
jaCUBE <jacube.cz@gmail.com>
2018-04-14 21:20:18 +0200

    Upraveny zbylé výskyty getenv().

--- bootstrap.php
+++ bootstrap.php
@@ -8,12 +8,12 @@ require 'app/fn.php'; // Helper funkce pro PHP
 // Načtení konfigurace před dotenv https://github.com/vlucas/phpdotenv (soubor .env)
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
-define('URL', getenv('URL')); // URL webu jako konstanta
+define('URL', env('URL')); // URL webu jako konstanta
 
 
 
 
-if(getenv('DEVELOPMENT')){ // Hlášení chyb ve vývojovém prosředí
+if(env('DEVELOPMENT')){ // Hlášení chyb ve vývojovém prosředí
     Tracy\Debugger::enable(); // Tracy ^_^ https://github.com/nette/tracy
     error_reporting(E_ERROR | E_PARSE);
 }else{
